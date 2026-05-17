8de117698c159d2082650d4a04d775defb4a13d7
raniakharrt <raniakharrat80@gmail.com>
2026-05-17 21:37:39 +0100

    rania

--- src/navbar1.jsx
+++ src/navbar1.jsx
@@ -4,7 +4,7 @@ export default function Appnavbar() {
 return (
 <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
 <Container>
-<Navbar.Brand href="#home">MyApp</Navbar.Brand>
+<Navbar.Brand href="#home">Rania</Navbar.Brand>
 <Navbar.Toggle aria-controls="basic-navbar-nav" />
 <Navbar.Collapse id="basic-navbar-nav">
 <Nav className="ms-auto">
